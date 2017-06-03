39e8dc1c6ae27cb4e93f8429eb90ce38d2f5c47b
ZiyingFeng <ziyingfeng@g.ucla.edu>
2017-06-03 03:39:41 -0700

    Update plot3.R

--- R_code/plot3.R
+++ R_code/plot3.R
@@ -23,6 +23,6 @@ points(data3$DateTime, data3$Sub_metering_2, type = "l", col = "red")
 
 points(data3$DateTime, data3$Sub_metering_3, type = "l", col = "blue")
 
-legend("topright", lty = c(1,1,1), col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex = 1, y.intersp = 0.5)
+legend("topright", lty = c(1,1,1), col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex = 1, y.intersp = 1)
 
 dev.off()
