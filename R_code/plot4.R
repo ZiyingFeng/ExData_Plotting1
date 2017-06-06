803e20d8ab3165eb43855fa4765c74bfc4f2e679
ZiyingFeng <ziyingfeng@g.ucla.edu>
2017-06-06 10:00:57 -0700

    Update plot4.R

--- R_code/plot4.R
+++ R_code/plot4.R
@@ -29,6 +29,8 @@ points(data3$DateTime, data3$Sub_metering_2, type = "l", col = "red")
 
 points(data3$DateTime, data3$Sub_metering_3, type = "l", col = "blue")
 
+# Or using lines(datetime, subMetering2, type="l", col="red")
+
 legend("topright", lty = c(1,1,1), col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex = 0.5, y.intersp = 0.5)
 
 plot(data3$DateTime, data3$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power")
